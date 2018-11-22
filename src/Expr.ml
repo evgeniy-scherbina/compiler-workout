d7f7b5a08e66b31b0735c2f2e14b645d8bccf4a9
evgeniy.scherbina <evgeniy.scherbina@bitfury.com>
2018-11-22 23:34:28 +0200

    Compilers. Homework 1

--- src/Expr.ml
+++ src/Expr.ml
@@ -36,12 +36,12 @@ let update x v s = fun y -> if x = y then v else s y
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
 (* Some testing; comment this definition out when submitting the solution. *)
-let _ =
-  List.iter
-    (fun x ->
-       try  Printf.printf "%s=%d\n" x @@ s x
-       with Failure s -> Printf.printf "%s\n" s
-    ) ["x"; "a"; "y"; "z"; "t"; "b"]
+(*let _ =*)
+  (*List.iter*)
+    (*(fun x ->*)
+       (*try  Printf.printf "%s=%d\n" x @@ s x*)
+       (*with Failure s -> Printf.printf "%s\n" s*)
+    (*) ["x"; "a"; "y"; "z"; "t"; "b"]*)
 
 (* Expression evaluator
 
@@ -50,5 +50,24 @@ let _ =
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+let bool_to_int value = if value then 1 else 0
+
+let int_to_bool value = if value == 0 then false else true
+
+let rec eval state_map expression = match expression with
+                                    | Const final_value -> final_value
+                                    | Var   var_name    -> (state_map var_name)
+                                    | Binop ("+", expr_1, expr_2) -> (eval state_map expr_1) + (eval state_map expr_2)
+                                    | Binop ("-", expr_1, expr_2) -> (eval state_map expr_1) - (eval state_map expr_2)
+                                    | Binop ("*", expr_1, expr_2) -> (eval state_map expr_1) * (eval state_map expr_2)
+                                    | Binop ("/", expr_1, expr_2) -> (eval state_map expr_1) / (eval state_map expr_2)
+                                    | Binop ("%", expr_1, expr_2) -> (eval state_map expr_1) mod (eval state_map expr_2)
+                                    | Binop ("<",  expr_1, expr_2) -> bool_to_int ((eval state_map expr_1) <  (eval state_map expr_2))
+                                    | Binop ("<=", expr_1, expr_2) -> bool_to_int ((eval state_map expr_1) <= (eval state_map expr_2))
+                                    | Binop (">",  expr_1, expr_2) -> bool_to_int ((eval state_map expr_1) >  (eval state_map expr_2))
+                                    | Binop (">=", expr_1, expr_2) -> bool_to_int ((eval state_map expr_1) >= (eval state_map expr_2))
+                                    | Binop ("==", expr_1, expr_2) -> bool_to_int ((eval state_map expr_1) == (eval state_map expr_2))
+                                    | Binop ("!=", expr_1, expr_2) -> bool_to_int ((eval state_map expr_1) != (eval state_map expr_2))
+                                    | Binop ("&&", expr_1, expr_2) -> bool_to_int ( int_to_bool (eval state_map expr_1) && int_to_bool (eval state_map expr_2))
+                                    | Binop ("!!", expr_1, expr_2) -> bool_to_int ( int_to_bool (eval state_map expr_1) || int_to_bool (eval state_map expr_2))
+                                    | Binop (any,  expr_1, expr_2) -> failwith (Printf.sprintf "WAT: %s" any)
